7dc5b858c33e211bccc84ce4e51c0f4a8b149ef3
chirag anand <chiraganand@live.com>
2020-01-16 14:47:18 +0530

    Updates

---
+++ precache-manifest.08eae994be956e3d13c0a2b1ef9b8367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b27c8bb76be83720a2234061cbb3d1b",
+    "url": "/React-to-do-app/index.html"
+  },
+  {
+    "revision": "2a550f6a25d65e314ff8",
+    "url": "/React-to-do-app/static/css/main.6d03d5c9.chunk.css"
+  },
+  {
+    "revision": "3233c9030f1f7c8db39d",
+    "url": "/React-to-do-app/static/js/2.f92e0c5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-to-do-app/static/js/2.f92e0c5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a550f6a25d65e314ff8",
+    "url": "/React-to-do-app/static/js/main.65c074ce.chunk.js"
+  },
+  {
+    "revision": "c1277c1264b80c01705e",
+    "url": "/React-to-do-app/static/js/runtime-main.a2442584.js"
+  }
+]);
