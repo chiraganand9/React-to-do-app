4614f4418197f3bae4702b097eb0ce4681480b5d
chirag anand <chiraganand@live.com>
2020-01-16 14:30:26 +0530

    Added Major changes like delete functionality and editing the list items and finalising the application

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,48 +1,69 @@
 import React from "react";
 import "./App.css";
 import ListItems from "./ListItems.js";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTrash);
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      items:[],
-      currentItem:{
-        text:'',
-        key:''
+    this.state = {
+      items: [],
+      currentItem: {
+        text: "",
+        key: ""
       }
     }
-    this.handleInput=this.handleInput.bind(this);
-    this.addItem=this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
 
-  handleInput(e){
+  handleInput(e) {
     this.setState({
-      currentItem:{
+      currentItem: {
         text: e.target.value,
         key: Date.now()
       }
     })
   }
 
-  addItem(e){
+  addItem(e) {
     e.preventDefault();
-    const newItem=this.state.currentItem;
+    const newItem = this.state.currentItem;
     console.log(newItem);
-    if(newItem.text!==""){
-      const newItems=[...this.state.items,newItem];
+    if (newItem.text !== "") {
+      const newItems = [...this.state.items, newItem];
       this.setState({
-        items:newItems,
-        currentItem:{
-          text:'',
-          key:''
+        items: newItems,
+        currentItem: {
+          text: "",
+          key: ""
         }
       })
     }
+  }
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item => item.key !== key);
+    this.setState({
+      items: filteredItems
+    })
+  }
+
+  setUpdate(text, key) {
+    const items = this.state.items;
+    items.map(item => {
+      if (item.key === key) {
+        item.text = text;
+      }
+    })
+    this.setState({
+      items: items
+    })
   }
 
   render() {
@@ -50,13 +71,20 @@ class App extends React.Component {
       <div className="App">
         <header>
           <form id="to-do-form" onSubmit={this.addItem}>
-            <input type="text" placeholder="Enter Text"
-            value={this.state.currentItem.text}
-            onChange={this.handleInput} />
+            <input
+              type="text"
+              placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
             <button type="submit">Add</button>
           </form>
         </header>
-        <ListItems items ={this.state.items}></ListItems>
+        <ListItems
+          items={this.state.items}
+          deleteItem={this.deleteItem}
+          setUpdate={this.setUpdate}
+        ></ListItems>
       </div>
     );
   }
--- todolist/src/ListItems.js
+++ todolist/src/ListItems.js
@@ -1,22 +1,39 @@
 import React from "react";
 import "./ListItems.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
 
 function ListItems(props) {
   const items = props.items;
   const listItems = items.map(item => {
     return (
-      <div className="list" key="item.key">
+      <div className="list" key={item.key}>
         <p>
-          {item.text}
+          <input
+            type="text"
+            id={item.key}
+            value={item.text}
+            onChange={e => {
+              props.setUpdate(e.target.value, item.key);
+            }}
+          />
           <span>
-            <FontAwesomeIcon className="faicons" icon="trash" />
+            <FontAwesomeIcon
+              className="faicons"
+              icon="trash"
+              onClick={() => props.deleteItem(item.key)}
+            />
           </span>
         </p>
       </div>
     );
   });
-  return <div>{listItems}</div>;
+  return(
+  <div>
+    <FlipMove duration={300} easing="ease-in-out">
+      {listItems}
+    </FlipMove>
+  </div>);
 }
 
 export default ListItems;
